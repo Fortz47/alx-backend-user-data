c454dec6c70681cbd3dbc017e54546c817cec41d
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2024-04-12 02:40:07 +0000

    task 5

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+"""A module to hash paswords with bcrypt"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """returns a hashed password"""
+    # Hash a password for the first time, with a randomly-generated salt
+    pwd = bytes(password, encoding='utf-8')
+    hashed = bcrypt.hashpw(pwd, bcrypt.gensalt())
+    return hashed
