998d0bcd1338384a1a34a3edc00ecb5a74af5472
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2024-04-16 16:16:47 +0000

    task 8 - 9

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Module to manage Basic Auth"""
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -16,3 +17,27 @@ class BasicAuth(Auth):
         except Exception:
             pass
         return None
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str
+                                          ) -> str:
+        """returns the decoded value of a Base64 string"""
+        try:
+            res = base64.b64decode(base64_authorization_header)
+            return res.decode('utf-8')
+        except Exception:
+            pass
+        return None
+
+    def extract_user_credentials(
+            self,
+            decoded_base64_authorization_header: str
+                                ) -> (str, str):
+        """returns the user email and password
+        from the Base64 decoded value"""
+        try:
+            username, pwd = decoded_base64_authorization_header.split(':', 1)
+            return username.strip(), pwd.strip()
+        except Exception:
+            pass
+        return None, None
