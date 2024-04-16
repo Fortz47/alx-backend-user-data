1f54915de2f65c35ceecde983ec4004eba8cc2c1
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2024-04-16 13:27:18 +0000

    task 1

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -4,7 +4,13 @@ Route module for the API
 """
 from os import getenv
 from api.v1.views import app_views
-from flask import Flask, jsonify, abort, request
+from flask import (
+                    Flask,
+                    jsonify,
+                    abort,
+                    request,
+                    make_response
+                  )
 from flask_cors import (CORS, cross_origin)
 import os
 
@@ -19,6 +25,16 @@ def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """ Unauthorized handler
+    """
+    return make_response(
+        jsonify({"error": "Unauthorized"}),
+        401
+    )
 
 
 if __name__ == "__main__":
--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,9 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('unauthorized', strict_slashes=False)
+def not_authorized() -> str:
+    """raise a 401 error by using abort"""
+    abort(401)
