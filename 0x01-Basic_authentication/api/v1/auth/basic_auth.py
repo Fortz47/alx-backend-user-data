521f031fca00870252024ab4588e121d1685587f
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2024-04-17 19:39:04 +0000

    task 10

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,8 @@
 """Module to manage Basic Auth"""
 from api.v1.auth.auth import Auth
 import base64
+from models.user import User
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -36,8 +38,27 @@ class BasicAuth(Auth):
         """returns the user email and password
         from the Base64 decoded value"""
         try:
-            username, pwd = decoded_base64_authorization_header.split(':', 1)
-            return username.strip(), pwd.strip()
+            user_email, pwd = decoded_base64_authorization_header.split(':', 1)
+            return user_email.strip(), pwd.strip()
         except Exception:
             pass
         return None, None
+
+    def user_object_from_credentials(
+            self,
+            user_email: str,
+            user_pwd: str
+                                    ) -> TypeVar('User'):
+        """eturns the User instance based on his email and password"""
+        try:
+            assert not (user_email is None or user_pwd is None)
+            assert isinstance(user_email, str) and isinstance(user_pwd, str)
+            assert User.count()
+            userList = User.search({'email': user_email})
+            assert len(userList)
+            user = userList[0]
+            assert user.is_valid_password(user_pwd)
+            return user
+        except Exception as e:
+            pass
+        return None
