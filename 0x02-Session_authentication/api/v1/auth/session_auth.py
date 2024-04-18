0fdf7f362e1ff91cf3903aadcf580ef1f4dac5a1
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2024-04-18 07:27:55 +0000

    task 3

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -9,12 +9,21 @@ class SessionAuth(Auth):
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
-        """creates a session id for a user id"""
+        """creates a session ID for a user ID"""
         try:
             assert user_id is not None
             assert isinstance(user_id, str)
         except Exception:
             return None
-        session_id = uuid.uuid4()
+        session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """returns a user ID based on session ID"""
+        try:
+            assert session_id is not None
+            assert isinstance(session_id, str)
+        except Exception:
+            return None
+        return self.user_id_by_session_id.get(session_id)
