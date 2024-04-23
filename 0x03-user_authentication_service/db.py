d52a5a78d58a881d166d20c030adfa8e42a3a2ad
Fortune Atuokwu <65767694+Fortz47@users.noreply.github.com>
2024-04-23 20:08:24 +0100

    Update db.py

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -46,7 +46,7 @@ class DB:
             raise NoResultFound
         return user
 
-    def update_user(self, user_id: int, **kwargs: dict) -> None:
+    def update_user(self, user_id: int, **kwargs) -> None:
         """updates a user based on a id in db"""
         user = self.find_user_by(id=user_id)
         for k, v in kwargs.items():
