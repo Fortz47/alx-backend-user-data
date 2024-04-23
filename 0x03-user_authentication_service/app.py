3a5b0276d81fb2befa1143a5dbb10aec1ffac5cf
Atuokwu Fortune <atuokwu.fortune@gmail.com>
2024-04-23 20:42:02 +0000

    task 6

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+"""simple flask app"""
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+
+app.route('/')
+def root():
+    """home route"""
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
